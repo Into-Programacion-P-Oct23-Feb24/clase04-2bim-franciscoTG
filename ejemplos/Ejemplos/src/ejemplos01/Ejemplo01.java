977860af132b59ac788289cac80fdcfc8a73870c
Francisco Torres <ft11056575@gmail.com>
2024-01-03 16:50:48 -0500

    Resuelto

--- ejemplos/Ejemplos/src/ejemplos01/Ejemplo01.java
+++ ejemplos/Ejemplos/src/ejemplos01/Ejemplo01.java
@@ -16,25 +16,54 @@ public class Ejemplo01 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
-        
-        String miCiudad = obtenerCiudadMayuscula("Loja");
-        System.out.printf("%s\n", miCiudad);
+
+        //el void  no tendra una valor de retorno
+        //String miCiudad = obtenerCiudadMayuscula("Loja");
+        //System.out.printf("%s\n", miCiudad);
+        // Procedimiento para volver a la tabla original
+        obtenerMultiplicacion(7,50);
+       String miMensaje =obtenerMultiplicaciondos(7,50);
+       String miMensajeFinal =obtenerMultiplicaciondos(7,50);
+       
+        System.out.printf("%s\n",miMensaje);
+
+        System.out.printf("%s\n", miMensaje);
+        System.out.printf("%s\n", miMensajeFinal);
+
     }
-    
-    
-    public String obtenerCiudadMayuscula(String m){
+
+    public static String obtenerCiudadMayuscula(String m) {
         String m2 = m.toUpperCase();
         return m2;
     }
-    
-    public static void obtenerMultiplicacion(int tabla, int limite){
-        
+    //aquial final se puso un return para que pueda de volver una cadena 
+     public static String obtenerMultiplicaciondos(int tabla, int limite) {
+        int multiplicacion = 0 ;
+        String cadenaFinal = "";
+        for (int i = 0; i < limite; i++) {
+            multiplicacion = tabla * i;
+            cadenaFinal = String.format("%s%d * %d = %d\n",
+                     cadenaFinal, tabla, i,
+                    multiplicacion);
+        }
+        return cadenaFinal;
+  }
+
+
+    public static void obtenerMultiplicacion(int tabla, int limite) {
+        int multiplicacion = 0 ;
+        String cadenaFinal = "";
+        for (int i = 0; i < limite; i++) {
+            multiplicacion = tabla * i;
+            cadenaFinal = String.format("%s%d * %d = %d\n",
+                     cadenaFinal, tabla, i,
+                    multiplicacion);
+        }
+        System.out.printf("%s\n", cadenaFinal);
     }
-    
-    public static String obtenerNombre(){
+
+    public static String obtenerNombre() {
         return "Luis";
     }
-    
-    
+
 }
--- ejemplos/Ejemplos/src/ejemplos02/Ejemplo041.java
+++ ejemplos/Ejemplos/src/ejemplos02/Ejemplo041.java
@@ -13,16 +13,23 @@ public class Ejemplo041 {
 
     public static void main(String[] args) {
         // 
-        obtenerMultiplicacion(primerValor, segundoValor); 
-        
+        int[][]arreglo1={{1,2,3},{3,2,4},{2,6,2}};
+        int[][]arreglo2={{1,2,3},{2,2,2},{3,1,2}};
+        int[][]arreglo3=new int[3][3];
+        for (int i=0;i<arreglo1.length;i++){
+            for (int j=0;i<arreglo1.length;i++)
+        }
+        obtenerMultiplicacion(primerValor, segundoValor);
+
     }
-        
-    public static void obtenerSuma(int a, int b){
-        int suma;
-        suma = a + b;
-        System.out.printf("El valor de la suma es: %d\n", suma);
-        
+
+    public static void obtenerMultiplicaion(int a, int b) {
+        int valor1 = 4;
+        int valor2 = 5;
+        int operacion;
+        operacion = a * b;
+        return operacion;
     }
-    
-    
+
 }
+//
